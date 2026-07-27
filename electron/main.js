8283955cdf9e7736f743dc8f142205ab0082b4f6
Zero-X-5 <2433616054@qq.com>
2026-07-27 10:47:19 +0800

    feat: add tray global shortcut and background behavior

--- electron/main.js
+++ electron/main.js
@@ -1,8 +1,29 @@
-import { app, BrowserWindow, screen, ipcMain } from 'electron';
+import { app, BrowserWindow, screen, ipcMain, Tray, Menu, globalShortcut } from 'electron';
 import path from 'node:path';
 import { readNotes, saveNote, createNote, watchNotes } from './drafts-store.js';
 
 let win;
+let tray;
+
+function showWindow() {
+  if (!win) return;
+  win.show();
+  win.focus();
+}
+
+function createTray() {
+  tray = new Tray(path.join(app.getAppPath(), 'assets/icon.png'));
+  tray.setToolTip('拾笺');
+
+  tray.setContextMenu(Menu.buildFromTemplate([
+    { label: '打开拾笺', click: showWindow },
+    { label: '新建草稿', click: () => win?.webContents.send('create-note-request') },
+    { type: 'separator' },
+    { label: '退出', click: () => app.quit() }
+  ]));
+
+  tray.on('double-click', showWindow);
+}
 
 function createWindow() {
   const display = screen.getPrimaryDisplay();
@@ -27,7 +48,14 @@ function createWindow() {
 
   win.loadFile('src/shijian-desktop.html');
 
-  watchNotes((change)=>{
+  win.on('close', (event) => {
+    if (!app.isQuitting) {
+      event.preventDefault();
+      win.hide();
+    }
+  });
+
+  watchNotes((change) => {
     win?.webContents.send('notes-changed', change);
   });
 }
@@ -40,8 +68,19 @@ ipcMain.handle('load-notes', () => readNotes());
 ipcMain.handle('save-note', (_, note) => saveNote(note));
 ipcMain.handle('create-note', () => createNote());
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+  createTray();
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
+  globalShortcut.register('CommandOrControl+Shift+Space', () => {
+    if (win?.isVisible()) win.hide();
+    else showWindow();
+  });
 });
+
+app.on('before-quit', () => {
+  app.isQuitting = true;
+  globalShortcut.unregisterAll();
+});
+
+app.on('window-all-closed', () => {});
