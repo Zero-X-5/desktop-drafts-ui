8f948ccacdac55ec85f26d9c033f838d04dcc014
Zero-X-5 <2433616054@qq.com>
2026-08-14 11:31:56 +0800

    feat: add liquid glass v8 renderer wrapper

---
+++ experiments/liquid-glass-winui3-v8-validation/LiquidGlassRenderer.cpp
@@ -0,0 +1,9 @@
+// GitHub connector-friendly source split. The .inc parts concatenate to the original V8 renderer.
+#include "LiquidGlassRenderer.part01.inc"
+#include "LiquidGlassRenderer.part02.inc"
+#include "LiquidGlassRenderer.part03.inc"
+#include "LiquidGlassRenderer.part04.inc"
+#include "LiquidGlassRenderer.part05.inc"
+#include "LiquidGlassRenderer.part06.inc"
+#include "LiquidGlassRenderer.part07.inc"
+#include "LiquidGlassRenderer.part08.inc"
